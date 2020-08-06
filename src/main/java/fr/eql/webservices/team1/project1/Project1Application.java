4259f838d51a213b32c506553c698dfa8795f110
Franck106 <francois612-resa@yahoo.fr>
2020-08-06 16:06:04 +0200

    first commit

---
+++ src/main/java/fr/eql/webservices/team1/project1/Project1Application.java
@@ -0,0 +1,22 @@
+package fr.eql.webservices.team1.project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Project1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1Application.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
