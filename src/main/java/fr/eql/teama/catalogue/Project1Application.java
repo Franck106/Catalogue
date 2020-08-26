c0897822f4b7771cf96bbdbecbc1079118635bdc
Franck106 <francois612-resa@yahoo.fr>
2020-08-26 09:00:06 +0200

    CAT-42 Bugfix errors support

--- src/main/java/fr/eql/teama/catalogue/Project1Application.java
+++ src/main/java/fr/eql/teama/catalogue/Project1Application.java
@@ -12,7 +12,7 @@ public class Project1Application {
 		SpringApplication app = new SpringApplication(Project1Application.class);
 		app.setAdditionalProfiles("initData");
 		ConfigurableApplicationContext context = app.run(args);
-		System.out.println("http://localhost:5000/hello");
+		System.out.println("http://localhost:5000/api/proposals");
 		//SpringApplication.run(Project1Application.class, args);
 	}
 
