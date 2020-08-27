dcebee24c24c0c3b5913fd66a832b8eded04508f
Pierre-Clément Kerneïs <pc.kerneis@gmail.com>
2020-08-27 14:58:09 +0200

    Fix initData and add private prop file to gitignore

--- src/main/java/fr/eql/teama/catalogue/Project1Application.java
+++ src/main/java/fr/eql/teama/catalogue/Project1Application.java
@@ -10,8 +10,7 @@ public class Project1Application {
 	public static void main(String[] args) {
 
 		SpringApplication app = new SpringApplication(Project1Application.class);
-		app.setAdditionalProfiles("initData");
-		app.setAdditionalProfiles("apikeys");
+		app.setAdditionalProfiles("initData", "apikeys");
 		ConfigurableApplicationContext context = app.run(args);
 		System.out.println("http://localhost:5000/api/proposals");
 		//SpringApplication.run(Project1Application.class, args);
