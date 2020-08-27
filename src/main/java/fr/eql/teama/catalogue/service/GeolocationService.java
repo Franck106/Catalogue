52dd2387a30d2f291997d49a90481d798753fd8f
Pierre-Clément Kerneïs <pc.kerneis@gmail.com>
2020-08-27 18:04:39 +0200

    Add distance criteria in proposal research

--- src/main/java/fr/eql/teama/catalogue/service/GeolocationService.java
+++ src/main/java/fr/eql/teama/catalogue/service/GeolocationService.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Service;
 @Service
 public interface GeolocationService {
     User addGeolocationToUser(User user);
+    double getDistance(String locationA, String locationB);
 }
